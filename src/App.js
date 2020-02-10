113d39b5b1d659fb29568878eaa58759a91b6c02
nayeemkazi <psubangladesh@gmail.com>
2020-02-10 16:55:13 +0600

    Added SIdeDrawer Scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Element } from "react-scroll";
 import "./resources/styles.css";
 import Header from "./components/header_footer/header";
 import Featured from "./components/featured/index";
@@ -12,11 +13,22 @@ function App() {
   return (
     <div className="App" style={{ height: "1500px" }}>
       <Header />
-      <Featured />
-      <VenueNFO />
-      <Highlights />
-      <Pricing />
-      <Location />
+      <Element name="featured">
+        <Featured />
+      </Element>
+
+      <Element name="venue">
+        <VenueNFO />
+      </Element>
+      <Element name="highlights">
+        <Highlights />
+      </Element>
+      <Element name="pricing">
+        <Pricing />
+      </Element>
+      <Element name="location">
+        <Location />
+      </Element>
       <Footer />
     </div>
   );
